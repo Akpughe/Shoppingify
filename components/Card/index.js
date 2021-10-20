7ca4323f5fc20c31f786b0c9278384389635937c
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-10-20 01:46:45 +0100

    card component

---
+++ components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {AiOutlinePlus} from 'react-icons/ai'
+const Card = ({fruitName}) => {
+    return (
+        <div className="flex justify-between items-center bg-white rounded-xl w-48 h-12 px-4 shadow-md ">
+            <div>
+                <span>{fruitName}</span>
+            </div>
+            <div>
+                <AiOutlinePlus size={20}/>
+            </div>
+        </div>
+    )
+}
+
+export default Card
